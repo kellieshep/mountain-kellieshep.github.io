a670978ebf1c351b1300a5ab26525c20f1384641
Kellie <35340387+kellieshep@users.noreply.github.com>
2018-06-04 20:49:42 -0600

    try

--- assignments/Lesson-7/scripts/windchill.js
+++ assignments/Lesson-7/scripts/windchill.js
@@ -2,5 +2,6 @@ function windChill(){
 var tempF = 81;
 var speed = 5; 
 var s = Math.pow(speed,0.16);
-var w = 35.74 +( 0.6215 * tempF) -( 35.75 * s) + (0.4275 * tempF * s);
-document.getElementById("windChill").innerHTML=w;} 
+var wchill = 35.74 +( 0.6215 * tempF) -( 35.75 * s) + (0.4275 * tempF * s);
+document.getElementById("wchill").innerHTML="The value is <strong>"+wchill.toFixed(1)+"</strong>";
+} 
