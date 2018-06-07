883c46f31343c23c03d910ef7e56e3b79901569e
Kellie <35340387+kellieshep@users.noreply.github.com>
2018-06-06 21:26:35 -0600

    fixes

--- assignments/Lesson-7/scripts/windchill.js
+++ assignments/Lesson-7/scripts/windchill.js
@@ -1,7 +1,8 @@
 function windChill(){
-var tempF = 81;
-var speed = 5; 
+var tempF = 80.9;
+var speed = 5;
 var s = Math.pow(speed,0.16);
-var wchill = 35.74 +( 0.6215 * tempF) -( 35.75 * s) + (0.4275 * tempF * s);
-document.getElementById("wchill").innerHTML="The value is <strong>"+ wchill.toFixed(1)+"</strong>";
-} 
+var w = 35.74 +( 0.6215 * tempF) -( 35.75 * s) + (0.4275 * tempF * s);
+var result= w.toFixed(1);
+document.getElementById("outputDiv").innerHTML= result; 
+}
