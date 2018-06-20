5e5e0da393389c1e6250219b20f26e7072ef5a35
Kellie <35340387+kellieshep@users.noreply.github.com>
2018-06-20 09:26:57 -0600

    weather page

--- assignments/lesson-9/js/weatherapi.js
+++ assignments/lesson-9/js/weatherapi.js
@@ -1,7 +1,10 @@
-var weatherRequest = new XMLHttpRequest(http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={786c31c2219f70a1504435aa6e57c6bc});
-request.open('GET', requestURL);
-        request.responseType = 'json';
-request.send();
+var weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET',http://api.openweathermap.org/data/2.5/weather?q=Franklin,ID &appid=786c31c2219f70a1504435aa6e57c6bc, true );
+        
+weatherRequest.send();
+weatherRequest.onload= function () {
+    var weatherInfo = JSON.parse(weatherRequest.responseText);
+    
+    console.log(weatherInfo);
+}
 
-
-{"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":295.96,"pressure":1022,"humidity":53,"temp_min":294.15,"temp_max":298.15},"visibility":10000,"wind":{"speed":5.7,"deg":250},"clouds":{"all":75},"dt":1529416200,"sys":{"type":1,"id":5091,"message":0.0051,"country":"GB","sunrise":1529379769,"sunset":1529439673},"id":2643743,"name":"London","cod":200}
