c75778e13f7a0503baa5fe0e0bfc33df9fcfd035
Kellie <35340387+kellieshep@users.noreply.github.com>
2018-07-16 10:46:38 -0600

    final site

--- assignments/Weathersite/scripts/windchill.js
+++
@@ -1,8 +0,0 @@
-function windChill(){
-var tempF = 80.9;
-var speed = 5;
-var s = Math.pow(speed,0.16);
-var w = 35.74 +( 0.6215 * tempF) -( 35.75 * s) + (0.4275 * tempF * s);
-var result= w.toFixed(1);
-document.getElementById("outputDiv").innerHTML= result; 
-}
